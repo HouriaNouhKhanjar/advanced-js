fdacceae0968f9043c0db6d96103aade85e516cd
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-02-24 23:33:42 +0100

    arrow function averge

--- arrow-functions.js
+++ arrow-functions.js
@@ -44,3 +44,41 @@ const multipleLine = () => (
 );
 
 console.log(multipleLine());
+
+
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    let sumMarks = 0;
+    let countMarks = 0;
+    for( stu of arr) {
+        if(stu.results[subject]){
+              sumMarks += stu.results[subject];
+              countMarks ++;
+        }
+    }
+        return sumMarks/countMarks;
+};
+
+let averageMarks = averagePoints(students, "maths");
+console.log(averageMarks);
